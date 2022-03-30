f246a2df907b9d9a8299e94512060f1c81c68b55
Achref9 <92304302+Achref9@users.noreply.github.com>
2022-03-30 11:10:18 +0100

    commit

---
+++ src/isetj/SessionServlet.java
@@ -0,0 +1,49 @@
+package isetj;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SessionServlet
+ */
+@WebServlet("/SessionServlet")
+public class SessionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SessionServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session= request.getSession();
+		Integer compteur =(Integer)session.getAttribute("compteur");
+	if
+	(compteur==null)
+		compteur=0;
+	compteur++;
+	session.setAttribute("compteur", compteur);
+	
+	response.getWriter().println("Vous avez visualisé cette page "+compteur+" foid depuis l'ouverture    de la session");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
